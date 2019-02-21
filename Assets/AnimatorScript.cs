5996ffd166ead37c1485ec8e668ec749c9fe80c8
Jaetriel <dafinskn@uni.coventry.ac.uk>
2019-02-21 12:38:49 +0000

    Final Zombie Game

---
+++ Assets/AnimatorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorScript : MonoBehaviour
+{
+   
+    Animator animator;
+    private HitEnemy enemyScript;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        enemyScript = GetComponentInChildren<HitEnemy>();
+        if (enemyScript != null)
+        {
+            enemyScript.isBoxing = false;
+        }
+    }
+
+
+    void Update()
+    {
+        bool boxing = Input.GetKey(KeyCode.B);
+        animator.SetBool("isBoxing", boxing);
+        if (enemyScript != null)
+        {
+            enemyScript.isBoxing = boxing;
+        }
+    }
+
+    
+}
+
+
+
---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public int numberOfZombies = 5;//How many zombies do we want in our game?
+
+    private GameObject[] allWaypoints;// list to store all waypoints
+    private GameObject zombiePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        zombiePrefab = GameObject.FindGameObjectWithTag("Zombie");
+        zombiePrefab.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int nrZombiesExist = GameObject.FindGameObjectsWithTag("Zombie").Length;
+        while (nrZombiesExist < numberOfZombies)
+        {
+
+            //instatiates a copy of zombiePrefab. I places it at the position of one of the waypoints (randomly picks an index in the waypoint array). It gives it a new rotation and adds the object that this script is attached to as parent
+            Instantiate(zombiePrefab, allWaypoints[Random.Range(0, allWaypoints.Length - 1)].transform.position, new Quaternion(), transform).SetActive(true);
+            //count them manually instead of calling FindGameObjectsWithTag every time
+            nrZombiesExist++;
+        }
+    }
+}
